50e2fca78e7c06de2cdb8d39cea0961885a626be
Damodar Lohani <dlohani48@gmail.com>
2021-09-26 14:03:28 +0545

    Initial Commit

---
+++ playground-for-apple/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  playground-for-apple
+//
+//  Created by Damodar Lohani on 26/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ playground-for-apple/playground_for_appleApp.swift
@@ -0,0 +1,17 @@
+//
+//  playground_for_appleApp.swift
+//  playground-for-apple
+//
+//  Created by Damodar Lohani on 26/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct playground_for_appleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
